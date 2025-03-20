7ea67c2f83d42ddf121463f39cf172866a9ed5a4
AlexandrAnatoliev <per-1986@list.ru>
2025-03-20 21:42:33 +0300

    fix: add version
    version 0.1.0

--- src/main/webapp/WEB-INF/classes/HomeServlet.java
+++ src/main/webapp/WEB-INF/classes/HomeServlet.java
@@ -2,16 +2,21 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**Class to return website home page*/ 
+/**
+ * Class to return website home page
+ *
+ * @version 0.1.0
+ * @author AlexandrAnatoliev
+ */
 public class HomeServlet extends HttpServlet {
 
-	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
-		try {
-			request.setAttribute("servletName", "homeServlet");
-			getServletConfig().getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    try {
+      request.setAttribute("servletName", "homeServlet");
+      getServletConfig().getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    }
+  }
 }
